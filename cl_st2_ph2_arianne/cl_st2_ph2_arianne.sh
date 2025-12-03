43385e897b7fab28ea14e6709ce2cae9114a9ba0
Rogério Yamada <eyamrog@gmail.com>
2025-12-03 10:08:35 -0300

    20251203-1008

--- cl_st2_ph2_arianne/cl_st2_ph2_arianne.sh
+++ cl_st2_ph2_arianne/cl_st2_ph2_arianne.sh
@@ -38,3 +38,9 @@ python clean_answers_human.py
 
 python tag.py
 # output: corpus/07_tagged
+
+python keylemmas.py \
+    --input corpus/07_tagged \
+    --output corpus/08_keylemmas \
+    --cutoff 3
+
